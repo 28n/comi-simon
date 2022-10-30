1bc74a9b1321f4608dd94c098479ce304cdda8c5
28n <28nh04@gmail.com>
2022-10-30 23:02:55 +0100

    more seo stuff

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -25,6 +25,7 @@ const Layout = ({ children, title }) => {
             <title>{t}</title>
             <meta name="twitter:title" content={t} />
             <meta property="og:title" content={t} />
+            <meta name="title" content={t} />
           </Head>
         )}
         {children}
--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,6 +8,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="title" content="Comi Simon - Buchautor" />
         <meta name="description" content="Comi Simon - Autor" />
         <meta name="author" content="Comi Simon" />
         <meta name="keywords" content="Comi Simon, comi simon, comisimon, comi, simon, Comi, Simon, Autor, Autor Comi Simon, Comi Simon Autor, Simon Comi Autor, Autor Simon Comi" />
@@ -18,8 +19,8 @@ const Main = ({ children, router }) => {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@comi_simon" />
         <meta name="twitter:creator" content="@comi_simon" />
-        <meta property="og:site_name" content="Comi Simon" />
-        <meta name="og:title" content="Comi Simon" />
+        <meta property="og:site_name" content="Comi Simon - Buchautor" />
+        <meta name="og:title" content="Comi Simon - Buchautor" />
         <meta property="og:type" content="website" />
         <title>Comi Simon</title>
       </Head>
